19c9b9bd1ef750f3a68edd9d7e2e8321e49d3fc7
Isaac Boorman <76838845+isaboo@users.noreply.github.com>
2021-05-14 07:46:31 +0100

    Update navigation.js

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,11 +1,11 @@
 function expand(button) {
-    document.getElementById("expand").className = "exandable";
+    document.getElementById("expand").className = "expand";
     document.getElementById("btn").onclick = close;
     document.getElementById("chevron").classList = "fas fa-chevron-circle-up";
     document.getElementById("body").style.overflow = "hidden";
 }
 function close(button) {
-    document.getElementById("expand").className = "hidden";
+    document.getElementById("expand").className = "expandhidden";
     document.getElementById("btn").onclick = expand;
     document.getElementById("chevron").classList = "fas fa-chevron-circle-down";
     document.getElementById("body").style.overflow = "scroll";
