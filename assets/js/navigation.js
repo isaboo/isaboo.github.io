a9b24168d4d18e96d3e743bace21e3aca4ceff3d
Isaac Boorman <76838845+isaboo@users.noreply.github.com>
2021-06-02 17:13:37 +0100

    fix searchbar issues

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -1,10 +1,12 @@
 function expand(button) {
     document.getElementById("expand").className = "expand";
     document.getElementById("btn").onclick = close;
+    document.getElementById("searchbar").className = "hidden";
     document.getElementById("chevron").classList = "fas fa-chevron-circle-up";
 }
 function close(button) {
     document.getElementById("expand").className = "expandhidden";
     document.getElementById("btn").onclick = expand;
+    document.getElementById("searchbar").className = "bar";
     document.getElementById("chevron").classList = "fas fa-chevron-circle-down";
 }
