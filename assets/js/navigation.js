b307186aba54e34cd49fe3069214e232b620824b
Isaac Boorman <76838845+isaboo@users.noreply.github.com>
2021-06-02 17:34:09 +0100

    add close button to results

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -2,11 +2,11 @@ function expand(button) {
     document.getElementById("expand").className = "expand";
     document.getElementById("btn").onclick = close;
     document.getElementById("searchbar").className = "hidden";
-    document.getElementById("chevron").classList = "fas fa-chevron-circle-up";
+    document.getElementById("chevron").classList = "fas fa-times";
 }
 function close(button) {
     document.getElementById("expand").className = "expandhidden";
     document.getElementById("btn").onclick = expand;
     document.getElementById("searchbar").className = "bar";
-    document.getElementById("chevron").classList = "fas fa-chevron-circle-down";
+    document.getElementById("chevron").classList = "fas fa-bars";
 }
